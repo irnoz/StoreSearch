6f3817c9453cd57aaf175b75e7c1e8fad6d39dd9
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-13 19:19:51 +0400

    feat: add search bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Irakli Nozadze on 13.06.23.
+//
+
+import Foundation
+
+class SearchResult {
+  var name = ""
+  var artistName = ""
+}
