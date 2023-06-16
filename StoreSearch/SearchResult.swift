767cbb50e932a9c300b667f4a1c643857904377c
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-16 15:58:27 +0400

    add json decoding

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -14,12 +14,28 @@ class ResultArray: Codable {
 
 class SearchResult: Codable, CustomStringConvertible {
   var description: String {
-    return "\nResult - Name: \(name), Artist Name: \(artistName ?? "None")"
+    return "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
   }
 
+  var kind: String? = ""
   var artistName: String? = ""
   var trackName: String? = ""
   var name: String {
     return trackName ?? ""
+  }
+  var trackPrice: Double? = 0.0
+  var currency = ""
+  var imageSmall = ""
+  var imageLarge = ""
+  var storeURL: String? = ""
+  var genre = ""
+  
+  enum CodingKeys: String, CodingKey {
+    case imageSmall = "artworkUrl60"
+    case imageLarge = "artworkUrl100"
+    case storeURL = "trackViewUrl"
+    case genre = "primaryGenreName"
+    case kind, artistName, trackName
+    case trackPrice, currency
   }
 }
