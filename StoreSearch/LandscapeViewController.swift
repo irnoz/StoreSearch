87c7a3836be7bcefa5f30f11f4d43f937be09e5a
irakli nozadze <iraklinozadze2509@gmail.com>
2023-06-21 23:20:19 +0400

    add landscape UI and image

--- StoreSearch/LandscapeViewController.swift
+++ StoreSearch/LandscapeViewController.swift
@@ -9,21 +9,39 @@ import UIKit
 
 class LandscapeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var pageControl: UIPageControl!
 
-        // Do any additional setup after loading the view.
-    }
-    
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
+    // Remove constraints from main view
+    view.removeConstraints(view.constraints)
+    view.translatesAutoresizingMaskIntoConstraints = true
+    // Remove constraints for page control
+    pageControl.removeConstraints(pageControl.constraints)
+    pageControl.translatesAutoresizingMaskIntoConstraints = true
+    // Remove constraints for scroll view
+    scrollView.removeConstraints(scrollView.constraints)
+    scrollView.translatesAutoresizingMaskIntoConstraints = true
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    view.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+    scrollView.contentSize = CGSize(width: 1000, height: 1000)
+  }
 
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    let safeFrame = view.safeAreaLayoutGuide.layoutFrame
+    scrollView.frame = safeFrame
+    pageControl.frame = CGRect(
+      x: safeFrame.origin.x,
+      y: safeFrame.size.height - pageControl.frame.size.height,
+      width: safeFrame.size.width,
+      height: pageControl.frame.size.height)
+  }
+
+  // MARK: - Navigation
+//  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//   
+//   }
 }
